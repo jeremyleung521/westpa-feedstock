fe8812f53b556eeb265360364f7c188d1965f742
Joshua L. Adelman <joshua.adelman@gmail.com>
2020-03-23 14:34:43 -0400

    update to latest version 2020.01

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,6 @@ cp -r ${SRC_DIR} ${PREFIX}/${PKG_NAME}-${PKG_VERSION}
 
 cd "${PREFIX}/${PKG_NAME}-${PKG_VERSION}"
 echo "$(ls -la)"
-WEST_PYTHON=$(which python2.7)
+WEST_PYTHON=$(which python3)
 WEST_PYTHON=$WEST_PYTHON $WEST_PYTHON .westpa_gen.py
 chmod +x westpa.sh
