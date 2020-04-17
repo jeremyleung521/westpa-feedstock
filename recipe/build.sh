fd7396bc71c7cf409e5623d812709d54c09c395c
Kim F. Wong <kimwong@pitt.edu>
2020-04-17 14:11:18 -0400

    Update to add env_var.sh to activate.d

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,3 +10,11 @@ echo "$(ls -la)"
 WEST_PYTHON=$(which python3)
 WEST_PYTHON=$WEST_PYTHON $WEST_PYTHON .westpa_gen.py
 chmod +x westpa.sh
+
+# Export WESTPA environment variables
+mkdir -p ${PREFIX}/etc/conda/{activate,deactivate}.d
+touch ${PREFIX}/etc/conda/{activate,deactivate}.d/env_vars.sh
+cat << EOC >> ${PREFIX}/etc/conda/activate.d/env_vars.sh
+#!/usr/bin/env bash
+. \$(dirname \$(dirname \`which python3\`))/${PKG_NAME}-${PKG_VERSION}/westpa.sh
+EOC
