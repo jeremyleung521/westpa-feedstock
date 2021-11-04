0c98d9203204819c0e000c271595411e9325f9f6
Jeremy Leung <jml230@pitt.edu>
2021-11-04 15:11:48 -0400

    added variant to run pytests post-install

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,12 +2,10 @@
 
 set -x
 
-cp -r ${SRC_DIR} ${PREFIX}/${PKG_NAME}-${PKG_VERSION}
-cd "${PREFIX}/${PKG_NAME}-${PKG_VERSION}"
+cd ${SRC_DIR}
+$PYTHON -m pip install --no-deps .
 
-python setup.py develop
-
-# Remove Previous WESTPA environment variables
+# Remove Previous WESTPA environment variables, if exists
 export PATH=:\${PATH}: # Wrap with colon to prevent special case
 export PATH=\${PATH//:\$WEST_BIN:} # Remove the Path
 export PATH=\${PATH#:} # Remove start colon
@@ -16,7 +14,7 @@ unset WEST_ROOT
 unset WEST_BIN
 unset WEST_PYTHON
 
-# Clean up of previously set codes in activate.d
+# Clean up of previously set codes in activate.d, if exists
 if [ -f ${PREFIX}/etc/conda/activate.d/env_vars.sh ]; then
     sed -i 's,'". ${PREFIX}/westpa-2020.05/westpa.sh"',,' ${PREFIX}/etc/conda/activate.d/env_vars.sh || :
     sed -i 's,'". ${PREFIX}/westpa-2020.03/westpa.sh"',,' ${PREFIX}/etc/conda/activate.d/env_vars.sh || :
